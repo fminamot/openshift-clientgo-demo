2caa91ae75971dceb3ae4b60bda0b4fd3f64b42c
Fusayuki Minamoto <minamoto@neverbird.net>
2025-11-30 19:22:47 +0900

    Removed unused functions

--- 05_informer_lister/lister/main.go
+++ 05_informer_lister/lister/main.go
@@ -1,20 +1,14 @@
 package main
 
 import (
-	"context"
-	"encoding/csv"
 	"flag"
 	"fmt"
-	"io"
 	"log"
-	"os"
 	"path/filepath"
 
-	projectv1 "github.com/openshift/api/project/v1"
 	projectclientset "github.com/openshift/client-go/project/clientset/versioned"
 	projectinformers "github.com/openshift/client-go/project/informers/externalversions"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
@@ -38,61 +32,6 @@ func getProjectClientSet() (*projectclientset.Clientset, error) {
 	clientset, err := projectclientset.NewForConfig(config)
 
 	return clientset, err
-}
-
-func createProjectRequest(name string, displayName string, description string) *projectv1.ProjectRequest {
-	projectRequest := projectv1.ProjectRequest{
-		ObjectMeta:  metav1.ObjectMeta{Name: name},
-		DisplayName: displayName,
-		Description: description,
-	}
-	return &projectRequest
-}
-
-func createProjectsFromCSV(clientset *projectclientset.Clientset, csvFile string) (map[string]int, error) {
-	pnames := map[string]int{}
-	counter := 0
-
-	file, err := os.Open(csvFile)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-	r := csv.NewReader(file)
-
-	if _, err := r.Read(); err != nil {
-		if err != io.EOF {
-			return nil, err
-		}
-	}
-
-	for {
-		row, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-
-		ctx := context.Background()
-
-		pr := createProjectRequest(row[0], row[1], row[2])
-
-		newProject, err := clientset.ProjectV1().ProjectRequests().Create(ctx, pr, metav1.CreateOptions{})
-		if err != nil {
-			return nil, err
-		}
-
-		pnames[newProject.Name] = counter
-		counter++
-
-		log.Printf("%s created\n", newProject.Name)
-	}
-	return pnames, nil
-}
-
-func printProject(p *projectv1.Project) {
-	displayName := p.Annotations["openshift.io/display-name"]
-	description := p.Annotations["openshift.io/description"]
-	fmt.Printf("%s\t%s\t%s\n", p.Name, displayName, description)
 }
 
 func main() {
